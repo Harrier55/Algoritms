0aa28e91e131bcfdc39f0f3c0a65c7b5d101f571
Harrier55 <harrier55@yandex.ru>
2022-06-07 22:18:36 +0500

    create  method hanoy tower

--- src/lesson5/main.java
+++ src/lesson5/main.java
@@ -7,22 +7,36 @@ package lesson5;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello Recursion");
-        int result = Recursion.arifmetic(2,3);
+        int result = Recursion.arifmetic(2, 3);
         System.out.println("результат  = " + result);
+
+        Recursion.hanoy(4, 1, 3, 2);
     }
 }
 
- class Recursion{
+class Recursion {
 
-    public static int arifmetic(int number, int stepen){  // задача 1
+    public int counter = 0;
+
+    public static int arifmetic(int number, int stepen) {  // задача 1
 
         int result = 1;
-        if( stepen > 0){
-            result = number * arifmetic(number,--stepen);
+        if (stepen > 0) {
+            result = number * arifmetic(number, --stepen);
             System.out.println(result);
         }
-            return result;
+        return result;
     }
 
+    /**ханойская башня*/
+
+    public static void hanoy(int n, int i, int p, int v) { // где i- источник, p-приемник, v-временная
+
+        if (n > 0) {
+            hanoy(n - 1, i, v, p);
+            System.out.println(i + "->" + p);
+            hanoy(n - 1, v, p, i);
+        }
+    }
 
 }
