90e1adb8e37677acdffb3ea1d6d8b04e6c3773ce
Harrier55 <harrier55@yandex.ru>
2022-06-09 00:09:29 +0500

    complete  class HorseGo

--- src/lesson5/main.java
+++ src/lesson5/main.java
@@ -1,8 +1,10 @@
 package lesson5;
 
 //Урок 5. Рекурсия
+
 //        1. Написать программу по возведению числа в степень с помощью рекурсии.
 //        2. *** Написать программу обхода шахматным конём доски (рекурсивно)
+
 
 public class main {
     public static void main(String[] args) {
@@ -10,41 +12,96 @@ public class main {
         int result = Recursion.arifmetic(2, 3);
         System.out.println("результат  = " + result);
 
-//        System.out.println("Tower");
+        System.out.println("Tower");
 
-//        Recursion.hanoy(4, 1, 3, 2); // ханойская башня
+        Recursion.hanoy(4, 1, 3, 2); // ханойская башня
 
         System.out.println("Horse");
 
-        HorseGo horseGo = new HorseGo(4);// задаем размер доски
-        horseGo.print_desk();
+        HorseGo horseGo = new HorseGo(6);// задаем размер доски
+        horseGo.start();
+
 
     }
 }
+/**  Задача 2 обход шахматным конём доски*/
+
+/**  Как то сделал. Сам не понял как заработало. Есмли можно  - поясните потом на уроке  */
 
 class HorseGo {
-    int x;
-    int y;
-    int[][] variki = {};
 
-    public HorseGo(int size){  // задаем размер доски
+    int x ;
+    int y ;
+    int counter = 0;
+    int[][] desk;
+    int[][] variants = {{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1},{-2,1},{-1,2}}; //вариант хода коня
+
+    public HorseGo(int size){
         this.x = size;
         this.y = size;
     }
 
-    public  void print_desk(){  // печатаем доску
+    public void start(){
+        createDesk();
+        setHorse(1,1); // передаем начальную координату коня на доске
+        print_desk();
+
+    }
+
+    private boolean setHorse(int x, int y){
+
+        // X и Y - начальная координата коня на доске
+
+        // проверка параметров функции
+        if(x < 0||x >= this.x||y < 0 || y >= this.y)  // находимся ли мы в пределах доски
+            return false;
+
+        if(desk[x][y] != 0)  //   коня можно ставить только на свободные клетки = 0
+            return false;
+
+        // остановка работы, решение найдено
+        if(counter == this.x * this.y)
+            return true;
+
+        // рукурсия
+        counter ++;
+        desk[x][y] = counter;
+        for (int i = 0; i < 8; i++){
+            int x1 = x + variants[i][0]; // вычисляем новую координату, куда переставить коня
+            int y1 = y + variants[i][1]; // то же
+          if(  setHorse(x1,y1))
+            return true;
+        }
+        return false;
+
+    }
+
+     private void createDesk(){  // создаем доску и заполняем ее нулями
+         desk = new int[x][y];
+        for (int i = 0; i < desk.length; i++) {
+            for (int j = 0; j < desk[i].length; j++) {
+                desk[i][j] = 0;
+            }
+        }
+    }
+
+
+       private void print_desk(){  // печатаем доску
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                System.out.print("0 ");
+                System.out.print(" "+ desk[i][j]);
             }
             System.out.println("");
         }
     }
 }
 
+
 class Recursion {
 
-    public static int arifmetic(int number, int stepen) {  // задача 1
+    /** Задача 1 степень числа  */
+
+    public static int arifmetic(int number, int stepen) {
 
         int result = 1;
         if (stepen > 0) {
@@ -57,8 +114,6 @@ class Recursion {
     /**ханойская башня*/
 
     public static void hanoy(int size, int from, int to, int temp) { // где from- источник, to-приемник, temp-временная
-
-//        System.out.println("итерация");
 
         if (size > 0) {
             hanoy(size - 1, from, temp, to);
