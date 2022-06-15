502461d021b387e2bd9e2f8a173d5641270adcdb
Harrier55 <harrier55@yandex.ru>
2022-06-16 01:22:13 +0500

    complete method way complete method showWays

--- src/lesson7/main.java
+++ src/lesson7/main.java
@@ -15,17 +15,20 @@ public class main {
         initGraf(graf);
 
         System.out.println("Начальный граф");
- //       graf.showGraf();
+        //       graf.showGraf();
 
         graf.bfc();
 
         System.out.println("Расчет минимального пути");
         graf.showGraf();
 
-        graf.way();
+        System.out.println("Расчет минимального маршрута");
+        graf.showWays();
+
+
     }
 
-    public static void initGraf(Graf graf){
+    public static void initGraf(Graf graf) {
         graf.addVertex('A');
         graf.addVertex('B');
         graf.addVertex('C');
@@ -38,35 +41,37 @@ public class main {
         graf.addVertex('Q');
 
 
-        graf.addEdge(0,1,1);
-        graf.addEdge(0,2,2);
-        graf.addEdge(0,3,3);
-        graf.addEdge(1,3,2);
-        graf.addEdge(1,4,6);
-        graf.addEdge(2,5,7);
-        graf.addEdge(3,5,3);
-        graf.addEdge(3,6,2);
-        graf.addEdge(4,7,7);
-        graf.addEdge(5,9,8);
-        graf.addEdge(5,6,5);
-        graf.addEdge(6,7,4);
-        graf.addEdge(6,9,10);
-        graf.addEdge(7,8,11);
-        graf.addEdge(7,9,15);
-        graf.addEdge(8,9,6);
+        graf.addEdge(0, 1, 1);
+        graf.addEdge(0, 2, 2);
+        graf.addEdge(0, 3, 3);
+        graf.addEdge(1, 3, 2);
+        graf.addEdge(1, 4, 6);
+        graf.addEdge(2, 5, 7);
+        graf.addEdge(3, 5, 3);
+        graf.addEdge(3, 6, 2);
+        graf.addEdge(4, 7, 7);
+        graf.addEdge(5, 9, 8);
+        graf.addEdge(5, 6, 5);
+        graf.addEdge(6, 7, 4);
+        graf.addEdge(6, 9, 10);
+        graf.addEdge(7, 8, 11);
+        graf.addEdge(7, 9, 15);
+        graf.addEdge(8, 9, 6);
     }
 }
 
-class Graf{
+class Graf {
     private final int MAX_VERTEX_SIZE = 10; // коичество вершин графа
     private final int INFINITY = 1_000_000_000; // имитируем бесконечность
     private Vertex vertexList[]; // массив вершин
     private int matrix[][]; // матрица связей графа
     private int counterVertex;  //счетчик для вершин
-    private Queue<Integer>queue;
+    private Queue<Integer> queue;
+
+    Stack<Integer> stackWay = new Stack();
 
     // конструктор
-    public Graf(){
+    public Graf() {
         counterVertex = 0;
         queue = new PriorityQueue<>();
         vertexList = new Vertex[MAX_VERTEX_SIZE];         // создаем массив для хранения вершин
@@ -78,45 +83,44 @@ class Graf{
         }
     }
 
+    public void showWays() {
+        for (int vetrex = 1; vetrex < vertexList.length; vetrex++) {
+            way(vetrex);
+        }
+    }
+
     // возвращаемся в вершину А через другие вершины по минимальному пути
-    public void way() {
-        Stack<Integer> stackWay = new Stack();
-
-        int initVertexIndex = 9; // вершин, откуда пойдем
-
-
-        System.out.println("Стартовая вершина =  " + vertexList[initVertexIndex].getLabel());
+    public void way(int vertexStart) {
+        int initVertexIndex = vertexStart; // вершин, откуда пойдем
+        stackWay.push(initVertexIndex);
+        char initVertex = vertexList[initVertexIndex].getLabel();
 
         while (initVertexIndex != 0) {
-
-
             for (int i = 0; i < MAX_VERTEX_SIZE; i++) {
                 int initVertexMarker = vertexList[initVertexIndex].getMarkerVertex();
                 int weightEdge = matrix[i][initVertexIndex];
                 int vertexMarker = vertexList[i].getMarkerVertex();
 
                 if (weightEdge != 0 && (initVertexMarker - weightEdge) == vertexMarker) {
-                    stackWay.add(i);
+                    stackWay.push(i);
                     initVertexIndex = i;
-
-                    System.out.println("--------------------------");
-                    System.out.println("Вершина  " + vertexList[i].getLabel() +
-                            " Ребро = " + weightEdge + " Маркер =  " + vertexMarker);
                 }
             }
-            System.out.println("target new  = " + vertexList[initVertexIndex].getMarkerVertex()
-                    + " для вершины " + vertexList[initVertexIndex].getLabel());
-
         }
+
+        // вывести путь на экран
+        System.out.println("Минимальный путь от вершины А  до " + initVertex);
+        while (!stackWay.isEmpty()) {
+            System.out.print(" " + vertexList[stackWay.pop()].getLabel());
+        }
+        System.out.println("");
     }
 
 
-
     //Поиск минимального значения пути от вершины А. Симбиоз алгоритма обход в ширину с урока с алгоритмом Дейкстры
-    public void bfc(){
+    public void bfc() {
         vertexList[0].setVisited(true);        // говорим что вершина просмотрена
         vertexList[0].setMarkerVertex(0);       // устанавливаем маркер вершины = 0
-        displayVertex(0);
         queue.add(0);
         int nextVertex;
 
@@ -131,13 +135,9 @@ class Graf{
                 int summa = markerPrevVertex + weightEdge; // суииа маркера пред.вершины и ребра
                 int markerNextVertex = vertexList[nextVertex].getMarkerVertex(); // маркер текущей вершины
 
-                if (summa < markerNextVertex){              // устанавливаем значение маркера в вершине
+                if (summa < markerNextVertex) {              // устанавливаем значение маркера в вершине
                     vertexList[nextVertex].setMarkerVertex(summa);
                 }
-//                displayVertex(prevVertex);
-//                displayVertex(nextVertex);
-//                System.out.println("вес ребра " + weightEdge   );
-
                 queue.add(nextVertex);
             }
 
@@ -145,8 +145,6 @@ class Graf{
                 vertexList[v].setVisited(false);
             }
         }
-
-
     }
 
     // проверяем, что к проверяемой вершине есть ребро (т.е вес > 0) и вершина еще не была посещена
@@ -168,19 +166,20 @@ class Graf{
     public void addEdge(int start, int end, int weight) {
         matrix[start][end] = weight; // задание ребер между вершинами, с весом между ними
     }
+
     // отобразить вершину
     public void displayVertex(int v) {
         System.out.println(vertexList[v].getLabel() + " + мин. путь  " + vertexList[v].getMarkerVertex());
     }
 
-    public void showGraf(){
+    public void showGraf() {
         for (int i = 0; i < vertexList.length; i++) {
             displayVertex(i);
         }
     }
 }
 
-class Vertex{
+class Vertex {
     private final int INFINITY = 1_000_000_000; // имитируем бесконечность
     private char label;         // обозначение вершины
     private boolean isVisited; // посещена ли вершина
