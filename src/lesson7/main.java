9d30e26b7a1959d53e791c9471e751225d3bd263
Harrier55 <harrier55@yandex.ru>
2022-06-15 20:28:20 +0500

    make class graf

--- src/lesson7/main.java
+++ src/lesson7/main.java
@@ -4,21 +4,33 @@ package lesson7;
 //        1. Реализовать программу, в которой задается граф из 10 вершин.
 //        Задать ребра и найти кратчайший путь с помощью поиска в ширину.
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class main {
     public static void main(String[] args) {
 
         Graf graf = new Graf();
+        initGraf(graf);
 
-        graf.addVertex('0');
-        graf.addVertex('1');
-        graf.addVertex('2');
-        graf.addVertex('3');
-        graf.addVertex('4');
-        graf.addVertex('5');
-        graf.addVertex('6');
-        graf.addVertex('7');
-        graf.addVertex('8');
-        graf.addVertex('9');
+        graf.showGraf();
+        System.out.println("____________________________");
+
+        graf.bfc();
+
+    }
+
+    public static void initGraf(Graf graf){
+        graf.addVertex('A');
+        graf.addVertex('B');
+        graf.addVertex('C');
+        graf.addVertex('F');
+        graf.addVertex('G');
+        graf.addVertex('E');
+        graf.addVertex('F');
+        graf.addVertex('J');
+        graf.addVertex('P');
+        graf.addVertex('Q');
 
 
         graf.addEdge(0,1,1);
@@ -31,17 +43,12 @@ public class main {
         graf.addEdge(3,6,2);
         graf.addEdge(4,7,7);
         graf.addEdge(5,9,8);
+        graf.addEdge(5,6,5);
         graf.addEdge(6,7,4);
         graf.addEdge(6,9,10);
         graf.addEdge(7,8,11);
         graf.addEdge(7,9,15);
         graf.addEdge(8,9,6);
-
-
-
-
-
-
     }
 }
 
@@ -50,36 +57,102 @@ class Graf{
     private final int INFINITY = 1_000_000_000; // имитируем бесконечность
     private Vertex vertexList[]; // массив вершин
     private int matrix[][]; // матрица связей графа
-
     private int counterVertex;  //счетчик для вершин
+    private Queue<Integer>queue;
 
+    // конструктор
     public Graf(){
         counterVertex = 0;
+        queue = new PriorityQueue<>();
         vertexList = new Vertex[MAX_VERTEX_SIZE];         // создаем массив для хранения вершин
         matrix = new int[MAX_VERTEX_SIZE][MAX_VERTEX_SIZE];// создаем матрицу смежности
-        for (int i = 0; i < MAX_VERTEX_SIZE; i++) {   // и заполняем ее значениями бесконечность
+        for (int i = 0; i < MAX_VERTEX_SIZE; i++) {   // и заполняем ее значениями 0
             for (int j = 0; j < MAX_VERTEX_SIZE; j++) {
-                matrix[i][j] = INFINITY;
+                matrix[i][j] = 0;
             }
         }
     }
 
+    // обход в ширину
+    public void bfc(){
+        vertexList[0].setVisited(true);        // говорим что вершина просмотрена
+        vertexList[0].setMarkerVertex(0);       // устанавливаем маркер вершины = 0
+        displayVertex(0);
+        queue.add(0);
+        int newVertex;
+
+        while (!queue.isEmpty()) {       // пока очередь не пуста крутимся по циклу
+            int v = queue.remove();     // достаем первый  элемент из очереди по принципу FIFO
+
+            while ((newVertex = getVertexInMatrix(v)) != -1) {  // проходим все смежные вершины
+
+                // логика подсчета
+                int summ = matrix[v][newVertex];
+
+                vertexList[newVertex].setVisited(true);     // говорим что вершина просмотрена
+
+                displayVertex(v);
+
+                displayVertex(newVertex);
+
+                System.out.println("вес ребра " + summ   );
+                queue.add(newVertex);
+            }
+
+        }
+
+
+    }
+    // проверяем, что к проверяемой вершине есть ребро (т.е вес > 0)
+    // и вершина еще не была посещена
+    private int getVertexInMatrix(int v) {
+        for (int i = 0; i < MAX_VERTEX_SIZE; i++) {
+            if (matrix[v][i] > 0 && vertexList[i].getIsVisited() == false) {
+                return i;  // возвращаем индекс вершины
+            }
+        }
+        return -1;  // если вершин не нашлось, возвращаем  -1
+    }
+
+    // добавить вершины в гарф
     public void addVertex(char label) {// задание новых вершин
         vertexList[counterVertex++] = new Vertex(label);
     }
 
+    // создать матрицу смежности
     public void addEdge(int start, int end, int weight) {
         matrix[start][end] = weight; // задание ребер между вершинами, с весом между ними
+    }
+    // отобразить вершину
+    public void displayVertex(int v) {
+        System.out.println(vertexList[v].getLabel() + "  маркер  " + vertexList[v].getMarkerVertex());
+    }
+
+    public void showGraf(){
+        for (int i = 0; i < vertexList.length; i++) {
+            displayVertex(i);
+        }
     }
 }
 
 class Vertex{
-    private char label;
-    private boolean isInTree;
+    private final int INFINITY = 1_000_000_000; // имитируем бесконечность
+    private char label;         // обозначение вершины
+    private boolean isVisited; // посещена ли вершина
+    private int markerVertex;
 
     public Vertex(char label) {
         this.label = label;
-        this.isInTree = false;
+        this.isVisited = false;
+        this.markerVertex = INFINITY;
+    }
+
+    public int getMarkerVertex() {
+        return markerVertex;
+    }
+
+    public void setMarkerVertex(int markerVertex) {
+        this.markerVertex = markerVertex;
     }
 
     public char getLabel() {
@@ -90,11 +163,11 @@ class Vertex{
         this.label = label;
     }
 
-    public boolean isInTree() {
-        return isInTree;
+    public boolean getIsVisited() {
+        return isVisited;
     }
 
-    public void setInTree(boolean inTree) {
-        isInTree = inTree;
+    public void setVisited(boolean visited) {
+        isVisited = visited;
     }
 }
